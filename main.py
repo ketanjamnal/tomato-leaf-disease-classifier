35b5b5f0e1653da3e7c45ac2bec5948618b6c44a
ketanjamnal <72164430+ketanjamnal@users.noreply.github.com>
2024-02-14 19:27:48 +0530

    update description

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide", page_title="Leaf Disease")
 
 st.write("## Leaf Disease Classifier")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog: Try uploading an image."
 )
 st.sidebar.write("## Upload and download :gear:")
 
